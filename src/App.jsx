a12933b5083445bee14f9f806dee413d8ea72359
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-19 05:39:44 +0600

    3.8 complex things, this was taugh need to repractice it

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import Form from "./components/Form";
-import Pointer from "./components/Pointer";
-import "./styles.css";
+import TravelPlan from "./components/TravelPlan";
 
 function App() {
-  const [color, setColor] = useState("green");
-  const handleChangeColor = () => {
-    setColor("black");
-  };
-  return (
-    <div>
-      <Form changeColor={color} />
-      <button onClick={handleChangeColor}>Click from parent</button>
-    </div>
-  );
+    return <TravelPlan />;
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,7 +8,24 @@ const initialItems = [
 
 export default function Menu() {
   const [items, setItems] = useState(initialItems);
-  const [selectedItem, setSelectedItem] = useState(items[0]);
+  const [selectedId, setSelectedId] = useState(0);
+
+  const selectedItem = items.find((item) => item.id === selectedId);
+
+  function handleItemChange(id, e) {
+    setItems(
+      items.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            title: e.target.value,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
 
   return (
     <>
@@ -16,10 +33,15 @@ export default function Menu() {
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            {item.title}{" "}
+            <input
+              value={item.title}
+              onChange={(e) => {
+                handleItemChange(item.id, e);
+              }}
+            />
             <button
               onClick={() => {
-                setSelectedItem(item);
+                setSelectedId(item.id);
               }}
             >
               Choose
