acb91fdf3db11e1a08d3d08796e7dbdea911cfc5
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-07-17 04:30:26 +0600

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import TaskApp from "./components/3.12/Main";
-import Page from "./components/3.16/Page";
-import ProfilePage from "./components/3.17/ProfilePage";
 import "./styles.css";
 
 const App = () => {
